80d5afc7f138373fef6bddcec66b1386812ab6b1
Gaurav-0801 <gaurav.pant2021@vitbhopal.ac.in>
2025-08-27 21:14:35 +0530

    deployment

--- backend/travel_booking/settings.py
+++ backend/travel_booking/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-default-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
 # Application definition
 INSTALLED_APPS = [
