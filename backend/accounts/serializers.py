09be890b7dc1166d2872e585d3e477067fa5cc1c
Gaurav-0801 <gaurav.pant2021@vitbhopal.ac.in>
2025-08-28 15:41:14 +0530

    deployment

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,7 +1,59 @@
+# accounts/serializers.py
+
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
 from bookings.models import Booking
 from travel_options.models import TravelOption
 
+User = get_user_model()
+
+# --------------------------
+# User Serializers
+# --------------------------
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+    password_confirm = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'password', 'password_confirm')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password_confirm']:
+            raise serializers.ValidationError({"password": "Passwords do not match."})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password_confirm')
+        user = User.objects.create_user(**validated_data)
+        return user
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        from django.contrib.auth import authenticate
+        user = authenticate(username=attrs['username'], password=attrs['password'])
+        if not user:
+            raise serializers.ValidationError("Invalid credentials.")
+        attrs['user'] = user
+        return attrs
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'first_name', 'last_name')
+
+class UserUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name')
+
+# --------------------------
+# Booking Serializers
+# --------------------------
 class BookingSerializer(serializers.ModelSerializer):
     travel = serializers.CharField(source='travel_option.id', read_only=True)
 
